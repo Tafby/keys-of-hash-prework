394e73ecacbefcee273b3d566424d62bce1337ee
Tiffany Carter <tafby88@gmail.com>
2018-11-13 05:15:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,11 @@ class Hash
   def keys_of(*arguments)
     array = []
   self.each do |key, value|
-    if arguments.includes?
+    if arguments.include?(value)
+      array << 
 
-    
-  end
+      end
+    end
   end
 end
 #h.keys_of("turtle", "cat", "dog")
